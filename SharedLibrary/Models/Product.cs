6e25805504b417fbf7c3feacf831e7fae92fd810
Alexandra Johansson <alexandra_johansson111@live.se>
2021-04-13 09:08:04 +0200

    updateringar db

--- SharedLibrary/Models/Product.cs
+++ SharedLibrary/Models/Product.cs
@@ -6,6 +6,11 @@ namespace SharedLibrary.Models
 {
     public partial class Product
     {
+        public Product()
+        {
+            Wishlists = new HashSet<Wishlist>();
+        }
+
         public int ProductId { get; set; }
         public int ModelId { get; set; }
         public int ColorId { get; set; }
