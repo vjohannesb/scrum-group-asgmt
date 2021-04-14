74d235ada128f6aeecbf6026e898da39ac9e127a
Michael Wong <michael.wong@learnet.se>
2021-04-14 21:03:58 +0000

    Merged PR 53: Validering för my account & contact us
    Blazor validering för my account & contact us + lite ändringar - tog bort javascript

--- SharedLibrary/Models/ViewModels/ContactModel.cs
+++ SharedLibrary/Models/ViewModels/ContactModel.cs
@@ -1,28 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
 
 namespace SharedLibrary.Models.ViewModels
 {
-    public class RegisterModel
+    public class ContactModel
     {
+        [Display(Name = "Category")]
+        [Required]
+        public string ProductCategory { get; set; }
         [Required]
         [MinLength(6, ErrorMessage = "A minimum of 4 characters is required")]
-        public string FirstName { get; set; }
-        [Required]
-        [MinLength(6, ErrorMessage = "A minimum of 4 characters is required")]
-        public string LastName { get; set; }
-
+        public string Name { get; set; }
         [Required]
         [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Please enter a valid email address.")]
         public string Email { get; set; }
-
         [Required]
-        [DataType(DataType.Password)]
-        [MinLength(8, ErrorMessage = "A minimum of 8 characters is required")]
-        public string Password { get; set; }
+        [MinLength(4, ErrorMessage = "A minimum of 4 characters is required")]
+        public string Subject { get; set; }
+        [Required]
+        [MinLength(30, ErrorMessage = "A minimum of 30 characters is required")]
+        public string Message { get; set; }
+
+
     }
 }
--- SharedLibrary/Models/ViewModels/SignInModel.cs
+++ SharedLibrary/Models/ViewModels/SignInModel.cs
@@ -1,8 +1,16 @@
-﻿namespace SharedLibrary.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SharedLibrary.Models.ViewModels
 {
     public class SignInModel
     {
+        [Required]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Please enter a valid email address.")]
         public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [MinLength(8, ErrorMessage = "A minimum of 8 characters is required")]
         public string Password { get; set; }
     }
 }
